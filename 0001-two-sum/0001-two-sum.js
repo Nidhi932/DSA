6f22c393e13019b3999b3d26bedee38e1c984b67
Nidhi Pandey <115703191+Nidhi932@users.noreply.github.com>
2025-09-06 17:01:20 +0530

    Time: 0 ms (100%), Space: 58.7 MB (5.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result=[];
-    for(let i=0;i<nums.length;i++){
-        for(let j=nums.length-1;j>=1;j--){
-            if(target-nums[i]== nums[j]){
-                if(i==j)continue;
-                else result.push(i,j);
-               return result;
-            }
+    const map = new Map(); // value -> index
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
         }
+        map.set(nums[i], i);
     }
-     
 };
